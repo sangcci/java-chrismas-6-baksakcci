4e0a15ab7658f3a619f1b88b910098fe2289f75f
baksakcci <pshsh910@gmail.com>
2023-11-11 18:27:42 +0900

    feat: 주문 메뉴 등록 기능 추가
    - 주문 메뉴가 존재하는 메뉴인지 검증
    - 메뉴 선택이 20개가 넘는지 검증

---
+++ src/main/java/christmas/domain/constant/Menu.java
@@ -0,0 +1,40 @@
+package christmas.domain.constant;
+
+import java.util.Arrays;
+
+public enum Menu {
+
+    MUSHROOM_SOUP("양송이스프", 6_000L),
+    TAPAS("타파스", 5_500L),
+    CAESAR_SALAD("시저샐러드", 8_000L),
+    T_BONE_STEAK("티본스테이크", 55_000L),
+    BARBECUE_RIBS("바비큐립", 54_000L),
+    SEAFOOD_PASTA("해산물파스타", 35_000L),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000L),
+    CHOCO_CAKE("초코케이크", 15_000L),
+    ICE_CREAM("아이스크림", 5_000L),
+    ZERO_COLA("제로콜라", 3_000L),
+    RED_WINE("레드와인", 60_000L),
+    CHAMPAGNE("샴페인", 25_000L);
+
+    private String name;
+    private long price;
+
+    Menu(String name, long price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // utility
+    public static Menu hasManu(String input) {
+        return Arrays.stream(values())
+                .filter(menu -> isExistMenu(input, menu.name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 메뉴입니다."));
+    }
+
+    // validation
+    private static boolean isExistMenu(String input, String name) {
+        return name.equals(input);
+    }
+}
---
+++ src/main/java/christmas/domain/order/OrderMenu.java
@@ -0,0 +1,40 @@
+package christmas.domain.order;
+
+import christmas.domain.constant.Menu;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class OrderMenu {
+
+    private final Map<Menu, Integer> orderMenuCount;
+
+    // constructor
+    private OrderMenu(Map<Menu, Integer> orderMenuCount) {
+        validateCountLimit(orderMenuCount);
+        this.orderMenuCount = orderMenuCount;
+    }
+
+    // static factory
+    public static OrderMenu of(Map<String, Integer> input) {
+        // convert to EnumMap
+        EnumMap<Menu, Integer> enumMap = new EnumMap<>(Menu.class);
+        input.forEach((k, v) -> {
+            Menu menu = Menu.hasManu(k);
+            enumMap.put(menu, v);
+        });
+        return new OrderMenu(enumMap);
+    }
+
+    // exception handling
+    private void validateCountLimit(Map<Menu, Integer> orderMenuCount) {
+        if (20 < getOrderMenuCountSum(orderMenuCount)) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 최대 20개까지 선택 가능하다.");
+        };
+    }
+
+    private int getOrderMenuCountSum(Map<Menu, Integer> orderMenuCount) {
+        return orderMenuCount.values().stream()
+                .mapToInt(i -> i)
+                .sum();
+    }
+}
