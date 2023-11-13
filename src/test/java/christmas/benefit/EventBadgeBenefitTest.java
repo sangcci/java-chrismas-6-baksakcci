34337da744dde32a7b80b4a09803b409d2abc6c2
baksakcci <pshsh910@gmail.com>
2023-11-13 14:24:36 +0900

    test: 이벤트 뱃지 혜택 기능 테스트 추가

---
+++ src/test/java/christmas/benefit/EventBadgeBenefitTest.java
@@ -0,0 +1,79 @@
+package christmas.benefit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.benefit.BenefitHistory;
+import christmas.domain.benefit.EventBadgeBenefit;
+import christmas.domain.constant.Benefit;
+import christmas.domain.constant.EventBadge;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class EventBadgeBenefitTest {
+
+    @Nested
+    @DisplayName("[service] 이벤트 뱃지 혜택을 적용한다")
+    class serviceTest {
+
+        @Test
+        @DisplayName("[SUCCESS] 총 혜택 금액이 5000원 이상 10000원 이하라면 별 뱃지를 받는다")
+        void should_getStarBadge_when_isBetween5000And10000() {
+            // given
+            BenefitHistory benefitHistory = BenefitHistory.of();
+            benefitHistory.addDiscountPrice(Benefit.WEEKEND_DISCOUNT, 5000);
+
+            // when
+            EventBadgeBenefit eventBadgeBenefit = EventBadgeBenefit.of();
+            EventBadge eventBadge = eventBadgeBenefit.apply(benefitHistory);
+
+            // then
+            assertThat(eventBadge).isEqualTo(EventBadge.STAR);
+        }
+
+        @Test
+        @DisplayName("[SUCCESS] 총 혜택 금액이 10000원 이상 20000원 이하라면 트리 뱃지를 받는다")
+        void should_getTreeBadge_when_isBetween10000And20000() {
+            // given
+            BenefitHistory benefitHistory = BenefitHistory.of();
+            benefitHistory.addDiscountPrice(Benefit.WEEKEND_DISCOUNT, 10000);
+
+            // when
+            EventBadgeBenefit eventBadgeBenefit = EventBadgeBenefit.of();
+            EventBadge eventBadge = eventBadgeBenefit.apply(benefitHistory);
+
+            // then
+            assertThat(eventBadge).isEqualTo(EventBadge.TREE);
+        }
+
+        @Test
+        @DisplayName("[SUCCESS] 총 혜택 금액이 20000원 이상 이라면 트리 뱃지를 받는다")
+        void should_getSantaBadge_when_isMoreThan20000() {
+            // given
+            BenefitHistory benefitHistory = BenefitHistory.of();
+            benefitHistory.addDiscountPrice(Benefit.WEEKEND_DISCOUNT, 20000);
+
+            // when
+            EventBadgeBenefit eventBadgeBenefit = EventBadgeBenefit.of();
+            EventBadge eventBadge = eventBadgeBenefit.apply(benefitHistory);
+
+            // then
+            assertThat(eventBadge).isEqualTo(EventBadge.SANTA);
+        }
+
+        @Test
+        @DisplayName("[SUCCESS] 총 혜택 금액이 5000원 이하 라면 뱃지 혜택을 받지 않는다")
+        void should_null_when_isLessThan5000() {
+            // given
+            BenefitHistory benefitHistory = BenefitHistory.of();
+            benefitHistory.addDiscountPrice(Benefit.WEEKEND_DISCOUNT, 1000);
+
+            // when
+            EventBadgeBenefit eventBadgeBenefit = EventBadgeBenefit.of();
+            EventBadge eventBadge = eventBadgeBenefit.apply(benefitHistory);
+
+            // then
+            assertThat(eventBadge).isNull();
+        }
+    }
+}
