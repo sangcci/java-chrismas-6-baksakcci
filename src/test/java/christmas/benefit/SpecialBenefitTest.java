f29ecd693c4dcc6aa567cba863971155011ae2dd
baksakcci <pshsh910@gmail.com>
2023-11-13 12:21:22 +0900

    test: 특별 할인 기능 테스트 추가

---
+++ src/test/java/christmas/benefit/SpecialBenefitTest.java
@@ -0,0 +1,51 @@
+package christmas.benefit;
+
+import christmas.domain.benefit.BenefitHistory;
+import christmas.domain.benefit.SpecialBenefit;
+import christmas.domain.constant.Benefit;
+import christmas.domain.order.OrderDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SpecialBenefitTest {
+
+    @Nested
+    @DisplayName("[service] 특별 할인 혜택을 적용한다")
+    class serviceTest {
+
+        @Test
+        @DisplayName("[SUCCESS] 별이 있는 날이라면 1000원 할인 받는다")
+        void should_discount_when_isStarDay() {
+            // given
+            OrderDate orderDate = OrderDate.of(10);
+            BenefitHistory benefitHistory = BenefitHistory.of();
+
+            // when
+            SpecialBenefit specialBenefit = SpecialBenefit.of();
+            specialBenefit.applyDiscount(orderDate, benefitHistory);
+
+            // then
+            assertThat(benefitHistory.getBenefitDiscountEachPrice(Benefit.SPECIAL_DISCOUNT))
+                    .isEqualTo(1_000L);
+        }
+
+        @Test
+        @DisplayName("[SUCCESS] 별이 없는 날이라면 할인 받지 않는다")
+        void should_notDiscount_when_isNotStarDay() {
+            // given
+            OrderDate orderDate = OrderDate.of(13);
+            BenefitHistory benefitHistory = BenefitHistory.of();
+
+            // when
+            SpecialBenefit specialBenefit = SpecialBenefit.of();
+            specialBenefit.applyDiscount(orderDate, benefitHistory);
+
+            // then
+            assertThat(benefitHistory.getBenefitDiscountEachPrice(Benefit.SPECIAL_DISCOUNT))
+                    .isEqualTo(0L);
+        }
+    }
+}
