c01258ecdd92e047222fb27e6cf5160915d9c41f
baksakcci <pshsh910@gmail.com>
2023-11-14 16:25:55 +0900

    refactor: 예외 처리 Supplier 적용

--- src/main/java/christmas/controller/ChristmasPromotionsController.java
+++ src/main/java/christmas/controller/ChristmasPromotionsController.java
@@ -1,15 +1,16 @@
 package christmas.controller;
 
 import christmas.domain.benefit.BenefitHistory;
-import christmas.service.DiscountService;
-import christmas.service.EventBadgeService;
-import christmas.service.GiftService;
 import christmas.domain.constant.EventBadge;
 import christmas.domain.order.OrderDate;
 import christmas.domain.order.OrderMenu;
 import christmas.domain.order.OrderPrice;
+import christmas.service.DiscountService;
+import christmas.service.EventBadgeService;
+import christmas.service.GiftService;
 import christmas.view.input.InputView;
 import christmas.view.output.OutputView;
+import java.util.function.Supplier;
 
 public class ChristmasPromotionsController {
 
@@ -31,18 +32,30 @@ public class ChristmasPromotionsController {
         this.eventBadgeService = eventBadgeService;
     }
 
+    public <T> T retryOnException(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println();
+            }
+        }
+    }
+
     public OrderDate inputOrderDate() {
         return OrderDate.of(inputView.inputDate());
     }
 
     public OrderMenu inputOrderMenu() {
-        return OrderMenu.of(inputView.inputOrderMenuAndCount());
+        return OrderMenu.of(inputView.inputMenu());
     }
 
     public void run() {
         // input
-        OrderDate orderDate = inputOrderDate();
-        OrderMenu orderMenu = inputOrderMenu();
+        inputView.openingComment();
+        OrderDate orderDate = retryOnException(this::inputOrderDate);
+        OrderMenu orderMenu = retryOnException(this::inputOrderMenu);
         OrderPrice orderPrice = OrderPrice.of(orderMenu);
         BenefitHistory benefitHistory = BenefitHistory.of();
 
